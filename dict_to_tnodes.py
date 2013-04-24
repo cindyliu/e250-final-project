426d3b3782351e26ea4fe187d74a806d83fda35d
Y. Jeanette Park <ypark@mba2013.hbs.edu>
2013-04-24 11:23:42 -0400

    fixes some layer stuff (between circnode to tnode, etc) that needed to happen

--- dict_to_tnodes.py
+++ dict_to_tnodes.py
@@ -2,7 +2,7 @@
 # and returns a doubly-linked ring of tnodes, one tnode for each unique url
 # with pointer to starting url and fields for each tnode set to the default values
 
-from fibheap_yjp_20130422_1200 import *
+from fibheap_yjp_20130423 import *
 from simple_crawler_modified import *
 
 # just assume for now that you will be passed a dict
@@ -13,23 +13,23 @@ def from_dict_to_fibheap(dict,starting_url) :
     is_start_url = True
     for key in dict:
         if key == starting_url:
-            key_tnode = TreeNode(key,0)
-            fibheap.insert(CircNode(Tree(key_tnode)))
+            key_tnode = TreeNode(0, key)
+            fibheap.insert(key_tnode)
             is_start_url = False
         else:
             # check if tnode already exists in circnode ring
             key_tnode = fibheap.find_on_self_url(key)        
             # if it doesn't, add it
             if key_tnode == None :
-                key_tnode = TreeNode(key, float("inf"))
-                fibheap.insert(CircNode(Tree(key_tnode)))
+                key_tnode = TreeNode(float("inf"), key)
+                fibheap.insert(key_tnode)
             # for each of the urls in its val, check if tnode already exists in circnode ring
         for val_url in dict[key]:
             val_tnode = fibheap.find_on_self_url(val_url)
             # if it doesn't exist, add the tnode
             if val_tnode == None:
-                val_tnode = TreeNode(val_url, float("inf"))
-                fibheap.insert(CircNode(Tree(val_tnode)))
+                val_tnode = TreeNode(float("inf"), val_url)
+                fibheap.insert(val_tnode)
             # the connect this val_url tnode to key tnode
             key_tnode.neighbors.append(val_tnode)
     return fibheap
